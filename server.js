f079eccfb80165f31460bda3bd88055dbffff14d
Konstantin Schiller <ks@schillerwebsolutions.com>
2023-08-19 21:40:12 -0400

    Added post functionality for new ideas

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = 5001;
 const app = express();
 
+// We need middleware for the post request - Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended:false}));
+
 app.get('/', (req, res) => {
     res.json({message: 'Welcome to the RandomIdeas API'}); // do not need to specify content type
 });
